ce7d6cfe08dd948c01ab768acb150a4da2e42be1
Nikita Rusin <nikita.rusin@indriver.com>
2025-03-21 17:26:41 +0200

    UI MPV

--- ui/theme.go
+++ ui/theme.go
@@ -1,3 +1,13 @@
 package ui
 
-var ()
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	purple    = lipgloss.Color("99")
+	gray      = lipgloss.Color("245")
+	lightGray = lipgloss.Color("241")
+
+	headerStyle = lipgloss.NewStyle().Foreground(purple).Bold(true).Align(lipgloss.Center)
+	cellStyle   = lipgloss.NewStyle().Padding(0, 1)
+	oddRowStyle = cellStyle.Foreground(gray)
+)
